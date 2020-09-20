0172d720817c569981187d4bab3ff9b443c91ff2
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-20 19:07:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,33 +7,41 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  fruit = [apple, pear, banana, coconut]
+  fruit.push(mango)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  fruit = [apple, pear, banana, coconut]
+  fruit.unshift(kenep)
 end
 
 def remove_element_from_end_of_array(array)
-
+  fruit = [apple, pear, banana, coconut]
+  fruit.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  fruit = [apple, pear, banana, coconut]
+  fruit.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  fruit = [apple, pear, banana, coconut]
+  fruit[3]
 end
 
 def retrieve_first_element_from_array(array)
-
+  fruit = [apple, pear, banana, coconut]
+  fruit[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  fruit = [apple, pear, banana, coconut] 
+  fruit[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  fruit = [apple, pear, banana, coconut]
+  fruit[1] = plum
 end
