e7f01cd8d0de13ddc31a102190d8488cf05be5f3
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-20 19:14:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,45 +3,45 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [apple, pear, banana, coconut]
+  fruit = ["apple", "pear", "banana", "coconut"]
 end
 
 def add_element_to_end_of_array(array, element)
-  fruit = [apple, pear, banana, coconut]
-  fruit.push(mango)
+  fruit = ["apple", "pear", "banana", "coconut"]
+  fruit.push("mango")
 end
 
 def add_element_to_start_of_array(array, element)
-  fruit = [apple, pear, banana, coconut]
-  fruit.unshift(kenep)
+  fruit = ["apple", "pear", "banana", "coconut"]
+  fruit.unshift("kenep")
 end
 
 def remove_element_from_end_of_array(array)
-  fruit = [apple, pear, banana, coconut]
+  fruit = ["apple", "pear", "banana", "coconut"]
   fruit.pop
 end
 
 def remove_element_from_start_of_array(array)
-  fruit = [apple, pear, banana, coconut]
+  fruit = ["apple", "pear", "banana", "coconut"]
   fruit.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  fruit = [apple, pear, banana, coconut]
+  fruit = ["apple", "pear", "banana", "coconut"]
   fruit[3]
 end
 
 def retrieve_first_element_from_array(array)
-  fruit = [apple, pear, banana, coconut]
+  fruit = ["apple", "pear", "banana", "coconut"]
   fruit[0]
 end
 
 def retrieve_last_element_from_array(array)
-  fruit = [apple, pear, banana, coconut]
+  fruit = ["apple", "pear", "banana", "coconut"]
   fruit[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  fruit = [apple, pear, banana, coconut]
-  fruit[1] = plum
+  fruit = ["apple", "pear", "banana", "coconut"]
+  fruit[1] = "plum"
 end
